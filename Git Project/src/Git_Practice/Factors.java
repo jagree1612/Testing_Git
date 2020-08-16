3626d9ae8bf5bb279790fdc9bc54fdb0ff4b9390
SAI <SAI@DESKTOP-IP68VSP>
2020-08-16 11:27:48 +0530

    commit 1

---
+++ Git Project/src/Git_Practice/Factors.java
@@ -0,0 +1,16 @@
+package Git_Practice;
+
+public class Factors {
+
+	public static void main(String args[]) {
+		
+		int i,No=9;
+		
+		for(i=1; i<=No; i++) {
+			
+			if (No%i==0)
+				
+				System.out.print(i+ " ");
+			}
+		}
+	}
